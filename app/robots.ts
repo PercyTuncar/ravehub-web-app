f4011a690258dcb2b266272a282a04158cfaf7a4
Percy Tuncar <percy.edgar.tuncar@gmail.com>
2025-11-08 12:49:26 -0300

    se desbloquea la pagina de compras del txt

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
-      { userAgent: '*', allow: '/', disallow: ['/admin/', '/api/', '/auth/', '/user/', '/tienda/carrito', '/tienda/checkout', '/eventos/*/comprar'] },
+      { userAgent: '*', allow: '/', disallow: ['/admin/', '/api/', '/auth/', '/user/', '/tienda/carrito', '/tienda/checkout'] },
     ],
     sitemap: 'https://www.ravehublatam.com/sitemap.xml',
   }
